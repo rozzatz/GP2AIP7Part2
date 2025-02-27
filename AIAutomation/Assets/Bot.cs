5b34813425351f9f33b205a90b5a43ceac3c5a74
Diego Alleyne <753825@student.dusd.net>
2025-02-27 14:21:50 -0800

    importyed assests

---
+++ AIAutomation/Assets/Bot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Bot : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+    }
+
+    void Seek(Vector3 location)
+    {
+        agent.SetDestination(location);
+    }
+
+    void Flee(Vector3 location)
+    {
+        Vector3 fleeVector = location - this.transform.position;
+        agent.SetDestination(this.transform.position - fleeVector);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Flee(target.transform.position);
+    }
+}
