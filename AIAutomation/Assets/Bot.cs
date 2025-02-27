89c8ff3c75627a6fda524b42963539626d77d909
Diego Alleyne <753825@student.dusd.net>
2025-02-27 15:26:29 -0800

    almost done with hide Part 1

--- AIAutomation/Assets/Bot.cs
+++ AIAutomation/Assets/Bot.cs
@@ -24,10 +24,64 @@ public class Bot : MonoBehaviour
         Vector3 fleeVector = location - this.transform.position;
         agent.SetDestination(this.transform.position - fleeVector);
     }
+    void pursue()
+    {
+        Vector3 targetDir = target.transform.position - this.transform.position;
+
+        float relativeHeading = Vector3.Angle(this.transform.forward, this.transform.TransformVector(target.transform.forward));
+        float toTarget = Vector3.Angle(this.transform.forward, this.transform.TransformVector(targetDir));
+
+        if(toTarget > 90 && relativeHeading < 20 && target.GetComponent<Drive>().currentSpeed < 0.01f)
+        {
+            Seek(target.transform.position);
+            return;
+        }
+
+        float lookAhead = targetDir.magnitude/(agent.speed + target.GetComponent<Drive>().currentSpeed);
+
+        Seek(target.transform.position + target.transform.forward * lookAhead);
+    }
+
+    void Evade()
+    {
+        Vector3 targetDir = target.transform.position - this.transform.position;
+        float lookAhead = targetDir.magnitude / (agent.speed + target.GetComponent<Drive>().currentSpeed);
+
+        Flee(target.transform.position + target.transform.forward * lookAhead);
+
+    }
+    Vector3 wanderTarget = Vector3.zero;
+    void Wander()
+    {
+       float WanderRadius = 10f;
+        float WanderDistance = 20f;
+        float WanderJitter = 1f;
+
+        wanderTarget += new Vector3(Random.Range(-1.0f,1.0f) * WanderJitter,
+                                                               0,
+                                                               Random.Range(-1.0f,1.0f) * WanderJitter);
+        wanderTarget.Normalize();
+        wanderTarget *= WanderRadius;
+
+        Vector3 targetLocal = wanderTarget + new Vector3(0, 0, WanderDistance);
+        Vector3 TargetWorld = this.gameObject.transform.InverseTransformVector(targetLocal);
+
+        Seek(TargetWorld);
+    }
+
+    void Hide()
+    {
+        float dist = Mathf.Infinity;
+        Vector3 chosenSpot = Vector3.zero;
+
+
+    }
 
     // Update is called once per frame
     void Update()
     {
-        Flee(target.transform.position);
+       
+            Wander();
+        
     }
 }
